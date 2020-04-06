2a37ee005ae247ff1611ae168f3e8ee529f027a3
YunusovSamat <samat.yunusov@bk.ru>
2020-04-06 22:09:07 +0300

    Chapter 6

--- app/models.py
+++ app/models.py
@@ -12,12 +12,18 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        return 'https://www.gravatar.com/avatar' \
+               f'/729e26a2a2c7ff24a71958d4aa4e5f35?s={size}'
 
     def __repr__(self):
         return f'<User {self.username}>'

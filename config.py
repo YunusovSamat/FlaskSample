168d80a9fb658939c7b20e5093646531840ad301
YunusovSamat <samat.yunusov@bk.ru>
2020-04-07 10:18:39 +0300

    Chapter 7

--- config.py
+++ config.py
@@ -8,3 +8,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'localhost'
+    MAIL_PORT = 8025
+    MAIL_USE_TLS = False
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
